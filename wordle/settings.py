30c0c1e226378cddc23f8b0a522e45d8dc4ad1a3
epicnoxious <jappreet16042002@gmail.com>
2023-04-13 11:45:04 +0530

    change db

--- wordle/settings.py
+++ wordle/settings.py
@@ -81,11 +81,14 @@ WSGI_APPLICATION = 'wordle.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'A1qWF9ZFVNi3E4qhbraz',
+        'HOST': 'containers-us-west-47.railway.app',
+        'PORT': '5524',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
